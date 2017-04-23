0281c5e9584b01d70c82c1efe5a70db2949df77f
Vítor Luís <vitorluis.mauricio@hotmail.com>
2017-04-23 05:21:22 -0300

    Main Menu
    Puzzle Panel Created
    Settings Panel Created
    Panel Animations Created
    Menu Controller Created

---
+++ Assets/Scripts/Controller Scripts/PuzzleMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleMenu : MonoBehaviour {
+
+	[SerializeField]
+	GameObject puzzlePanel;
+	[SerializeField]
+	Animator puzzleAnimator;
+
+	string selectedPuzzle;
+
+	public void SelectPuzzle() {
+		selectedPuzzle = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+		Debug.Log (selectedPuzzle);
+	}
+}
---
+++ Assets/Scripts/Controller Scripts/SettingsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsController : MonoBehaviour {
+
+	[SerializeField]
+	GameObject settingsPanel;
+	[SerializeField]
+	Animator settingsAnimator;
+
+	public void OpenSettings() {
+		settingsPanel.SetActive (true);
+		settingsAnimator.Play ("SlideIn");
+	}
+
+	public void CloseSettings() {
+		StartCoroutine (CloseSettingsRoutine ());
+	}
+
+	IEnumerator CloseSettingsRoutine() {
+		settingsAnimator.Play ("SlideOut");
+		yield return new WaitForSeconds (.5f);
+		settingsPanel.SetActive (false);
+	}
+}
